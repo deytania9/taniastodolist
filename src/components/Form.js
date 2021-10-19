c9be59507547bfbc1e7415ab02acda62301ffb86
deytania9 <53503945+deytania9@users.noreply.github.com>
2021-10-19 08:52:07 +0530

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -18,14 +18,14 @@ const Form=({setInputText, todos, setTodos,inputText})=>{
       <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
-      <div className="select">
+{/*<div className="select">
         <select 
         name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="incomplete">Incomplete</option>
         </select>
-      </div>
+      </div>*/}
     </form>
     )
 }
